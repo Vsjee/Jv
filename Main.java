d9dce74158c2b484a49ba8ae8f58772ec5f945d2
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-06 21:26:33 -0500

    use frame and start threat

--- Main.java
+++ Main.java
@@ -1,12 +1,24 @@
+import java.awt.LayoutManager;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import Recursion.Recursion;
 import SongTask.SongTask;
+import jFrame.Frame;
 
 public class Main {
   public static void main(String[] args) {
+  
     Recursion recursionTask = new Recursion();
     recursionTask.start();
 
     SongTask songTask = new SongTask();
     songTask.start();
+
+    Frame javaFrame = new Frame();
+    javaFrame.start();
   }
 }
